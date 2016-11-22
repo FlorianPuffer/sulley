5825095c4010617b1d5a9b264cff81272c6d1e43
FP <pufferflorian@gmail.com>
2016-11-22 21:47:03 +0100

    add new blocks add automatic device search via device name add external monitoring of processes via adb to android

--- examples/fuzz_blue.py
+++ examples/fuzz_blue.py
@@ -1,15 +1,115 @@
+import struct
+import time
+import os
+import bluetooth._bluetooth as bluez
+import bluetooth as pybluez
 
 from sulley   import *
-import btsocket as bt
 from requests import bluetooth
+
+serial_id_device = "XXXXXXXXXX"                                                #define Android ID for adb debugging here
+
+def _toHex(s):
+    lst = []
+    for ch in s:
+        hv = hex(ord(ch)).replace('0x', '')
+        if len(hv) == 1:
+            hv = '0' + hv
+        lst.append(hv)
+    return reduce(lambda x, y: x + y, lst)
+
+def search_device(sess, target_name):
+
+    sess.bluetoothSocket.send("\x01\x0c\x20\x02\x01\x00")                           #Enable LE scan
+
+    results = []
+    done = False
+    while not done:
+        pkt = sess.bluetoothSocket.recv(255)
+        ptype, event, plen = struct.unpack("BBB", pkt[:3])
+        if event == 62:
+            pkt = pkt[3:]
+            nrsp = pybluez.get_byte(pkt[0])
+            for i in range(nrsp):
+                addr = bluez.ba2str(pkt[4 + 6 * i:4 + 6 * i + 6])
+                if target_name in pkt:
+                    print(" %s found with mac %s" %(target_name,addr))
+                    sess.bluetoothSocket.send("\x01\x0c\x20\x02\x00\x00")           #Disable LE scan
+                    done = True
+                    time.sleep(2)
+                    return pkt[4 + 6 * i:4 + 6 * i + 6]                             #Return as hex
+                results.append((addr, -1))
+                print("[%s] (no RRSI)" % addr)
+
+        else:
+            print("unrecognized packet type 0x%02x" % ptype)
+        #print("event ", event)
+
+
+def connect_to_device(sess, target_mac):
+    # Setup before fuzzing session
+    sess.bluetoothSocket.send(
+        "\x01\x0d\x20\x19\x60\x00\x30\x00\x00\x01" + target_mac + "\x00\x18\x00\x28\x00\x00\x00\xbc\x02\x00\x00\x00\x00")
+
+    done = False
+    # TODO refactor HCI event handler
+    while not done:
+        pkt = sess.bluetoothSocket.recv(512)
+        ptype, event, plen = struct.unpack("BBB", pkt[:3])
+        if (ptype == 0x04):
+            if event == 0x3E:
+                print("EVT_LE_META Connection complete")
+                done=True
+            else:
+                print("unhandled Event")
+
+    time.sleep(3)
+
+def bt_service_is_alive():
+
+    #android platform tools path
+    platform_tools_path="/home/student/Desktop/android-sdk-linux/platform-tools"
+
+    #check that the target processes still alive
+    _, stdout = os.popen2(platform_tools_path + '/adb -s ' + serial_id_device +' shell pgrep /system/bin/brcm-uim-sysfs')
+    pid = stdout.read()
+    if pid == '':
+        return ''
+
+    _, stdout = os.popen2(platform_tools_path + '/adb -s ' + serial_id_device +' shell pgrep com.android.bluetooth')
+    pid = stdout.read()
+    if pid == '':
+        return ''
+
+    _, stdout = os.popen2(platform_tools_path + '/adb -s ' + serial_id_device +' shell pgrep system_server')
+    pid = stdout.read()
+    if pid == '':
+        return ''
+
+    return pid
+
+def bt_service_restart():
+    '''Restart the target. Called when instrumentation (post) fail.'''
+    print ("BLUETOOTH SERVICE CRASH!!!!!!!!!!")
+    #os.popen2('ssh %s /etc/init.d/target restart' % IP_DST)
 
 
 def do_fuzz ():
+    #setup monitoring
     sess   = sessions.session(proto="bluetooth")
-    target = sessions.target('4BEE7037B840', 6666)
-
+    target = sessions.target("127.0.0.1", 0)
+    #target.netmon = pedrpc.client("127.0.0.1", 26001)                                  #network monitor is not working due no support of bluetooth decoding in impackt module
+    target.procmon = instrumentation.external(post=bt_service_is_alive, start=bt_service_restart)
     sess.add_target(target)
-    sess.connect(s_get("1234"))
+
+    #find and connect device
+    target_name = "target"
+    target_mac=search_device(sess, target_name)
+    connect_to_device(sess, target_mac)
+
+    #setup fuzzing
+    sess.connect(s_get("le_test_block"))
+    #sess.connect(s_get("le_read_by_group_type_request_1"))
 
     sess.fuzz()
 
@@ -19,3 +119,4 @@ def do_fuzz ():
 
 
 do_fuzz()
+
--- requests/bluetooth.py
+++ requests/bluetooth.py
@@ -17,7 +17,7 @@ def rpc_request_encoder (data):
     return data
 
 ########################################################################################################################
-s_initialize("1234")
+s_initialize("brd_connect")
 s_block_start("connect", encoder=rpc_request_encoder)
 
 s_static("\x01")                                                                          #HCI command opcode
@@ -38,8 +38,183 @@ s_static("\x01")                                                                
 
 s_block_end()
 
+########################################################################################################################
 
+
+########################################################################################################################
+s_initialize("ble_connect")
+s_block_start("connect", encoder=rpc_request_encoder)
+
+s_static("\x01")                                                                          #HCI command opcode
+s_static("\x20\x0D")                                                                      #Create connection opcode
+
+s_static("\x19")                                                                          #Parameter total length Length
+s_static("\x60\x00")                                                                      #Scan Interval
+s_static("\x30\x00")                                                                      #Scan Window
+s_static("\x00")                                                                          #Initiator Filter Policy
+s_static("\x01")                                                                          #Peer Address Type: Random Device Adress
+s_static("\xAE\x74\x78\xA7\xD0\x64")                                                      #MAC adress of the target
+
+s_static("\x18\x00")                                                                      #Connection Interval Min
+s_static("\x28\x00")                                                                      #Connection Interval Miax
+s_static("\x00\x00")                                                                      #Connection Latency
+s_static("\xBC\x02")                                                                      #Supervision Timeout
+
+s_static("\x00\x00")                                                                      #Min CE length
+s_static("\x00\x00")                                                                      #Max CE length
+
+
+
+s_block_end()
+
+########################################################################################################################
+
+########################################################################################################################
+s_initialize("le_read_remote_used_features")
+s_block_start("read", encoder=rpc_request_encoder)
+
+s_static("\x01")                                                                          #HCI command opcode
+s_static("\x16\x20")                                                                      #Create connection opcode
+
+s_static("\x02")                                                                          #Parameter total length Length
+s_static("\x40\x00")                                                                      #Connection Handle
+
+
+s_block_end()
 
 ########################################################################################################################
 
 
+########################################################################################################################
+s_initialize("le_read_by_group_type_request_1")
+s_block_start("read", encoder=rpc_request_encoder)
+
+s_static("\x02")                                                                          #HCI ACL Type
+s_static("\x40\x20")                                                                      #Create connection opcode
+
+s_static("\x0B\x00")                                                                      #Parameter total length Length
+
+#L2CAP
+s_static("\x07\x00")                                                                      #L2CAP payload length
+s_static("\x04\x00")                                                                      #CID: Attribute Protocol
+
+#Bluetooth Attribute Prtocol
+s_static("\x10")                                                                          #Opcode
+s_static("\x01\x00")                                                                      #Starting Handle: 0x0001
+s_static("\xff\xff")                                                                      #Ending Handle: 0xFFFF
+s_static("\x00\x28")                                                                      #UUID: GATT Primary Service Declaration
+
+
+s_block_end()
+
+########################################################################################################################
+
+########################################################################################################################
+s_initialize("le_read_by_group_type_request_30")
+s_block_start("read", encoder=rpc_request_encoder)
+
+s_static("\x02")                                                                          #HCI ACL Type
+s_static("\x40\x20")                                                                      #Create connection opcode
+
+s_static("\x0B\x00")                                                                      #Parameter total length Length
+s_static("\x40\x00")                                                                      #Connection Handle
+
+#L2CAP
+s_static("\x07\x00")                                                                      #L2CAP payload length
+s_static("\x04\x00")                                                                      #CID: Attribute Protocol
+
+#Bluetooth Attribute Prtocol
+s_static("\x10")                                                                          #Opcode
+s_static("\x30\x00")                                                                      #Starting Handle: 0x0001
+s_static("\xff\xff")                                                                      #Ending Handle: 0xFFFF
+s_static("\x00\x28")                                                                      #UUID: GATT Primary Service Declaration
+
+
+s_block_end()
+
+########################################################################################################################
+
+
+########################################################################################################################
+s_initialize("le_read_by_group_type_request_list2")
+s_block_start("read", encoder=rpc_request_encoder)
+
+s_static("\x02")                                                                          #HCI ACL Type
+s_static("\x40\x20")                                                                      #Create connection opcode
+
+s_static("\x12\x00")                                                                      #Parameter total length Length
+
+#L2CAP
+s_static("\x0E\x00")                                                                      #L2CAP payload length
+s_static("\x04\x00")                                                                      #CID: Attribute Protocol
+
+#Bluetooth Attribute Prtocol
+s_static("\x11")                                                                          #Opcode
+s_static("\x06")                                                                          #Length of Attribute Data
+s_static("\x01\x00")                                                                      #Handel: Generic Attribute Profile
+s_static("\x05\x00")                                                                      #Group End Handle
+s_static("\x01\x18")                                                                      #UUID: Generic Attrbute Profile
+
+s_static("\x14\x00")                                                                      #Handel: Generic Attribute Profile
+s_static("\xFF\xFF")                                                                      #Group End Handle
+s_static("\x00\x18")                                                                      #UUID: Generic Attrbute Profile
+
+s_block_end()
+
+########################################################################################################################
+
+
+
+
+
+
+
+
+
+########################################################################################################################
+s_initialize("le_sent_connection_parameter_update")
+s_block_start("read", encoder=rpc_request_encoder)
+
+s_static("\x02")                                                                          #HCI ACL Type
+s_static("\x40\x20")                                                                      #Create connection opcode
+
+s_static("\x0A\x00")                                                                      #Parameter total length Length
+
+#L2CAP
+s_static("\x06\x00")                                                                      #L2CAP payload length
+s_static("\x05\x00")                                                                      #CID: LOW Energy L2CAP Signaling Channel
+
+#Connection Parameter update response
+s_static("\x13")                                                                          #Command Code: Connection Paramter Update Response
+s_static("\x02")                                                                          # Command identifier
+
+s_static("\x02\x00")                                                                      #Command length
+s_static("\x00\x00")                                                                      #Move Result: Accepted
+
+
+s_block_end()
+
+########################################################################################################################
+
+########################################################################################################################
+s_initialize("le_test_block")
+
+s_static("\x02")                                                                          #HCI ACL Type
+s_static("\x48\x20")                                                                      #Create connection opcode
+
+s_size("ParamterLength", length=2)                                                         #Parameter total length Length
+
+if s_block_start("ParamterLength"):
+    #L2CAP
+    s_static("\x07\x00")                                                                      #L2CAP payload length
+    s_static("\x04\x00")                                                                      #CID: Attribute Protocol
+
+    #Bluetooth Attribute Prtocol
+    s_byte("\x10", fuzzable=True)                                                             #Opcode
+    s_static("\x01\x00")                                                                      #Starting Handle: 0x0001
+    s_static("\xff\xff")                                                                      #Ending Handle: 0xFFFF
+    s_static("\x00\x28")                                                                      #UUID: GATT Primary Service Declaration
+s_block_end()
+
+
+########################################################################################################################
